5791bc0463fb6b2347a2a960b94b3af391fbcff3
waykar-vaishnavi <waykarvaishnavi144@gmail.com>
2024-03-12 21:49:06 +0530

    Customer Add Api Commit

--- LoanApplication/src/main/java/com/loan/app/repository/LoanAppRepository.java
+++ LoanApplication/src/main/java/com/loan/app/repository/LoanAppRepository.java
@@ -2,7 +2,11 @@ package com.loan.app.repository;
 
 import org.springframework.stereotype.Repository;
 
+import com.loan.app.model.CustomerDetails;
+
 @Repository
 public interface LoanAppRepository {
+
+	public void save(CustomerDetails cd);
 
 }
--- LoanApplication/src/main/java/com/loan/app/service/LoanAppService.java
+++ LoanApplication/src/main/java/com/loan/app/service/LoanAppService.java
@@ -1,5 +1,12 @@
 package com.loan.app.service;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import com.loan.app.model.CustomerDetails;
+
 public interface LoanAppService {
+
+	public CustomerDetails saveCustomer(String customerDetails, MultipartFile pancard, MultipartFile incomeCertificate,
+			                            MultipartFile adharcard, MultipartFile pphoto, MultipartFile ssign, MultipartFile bankpassbook);
 
 }
