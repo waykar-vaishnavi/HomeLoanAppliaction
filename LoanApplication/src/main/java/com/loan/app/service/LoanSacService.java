2e1dc77a933fab984e9d4f3c798d861ff30e723e
waykar-vaishnavi <waykarvaishnavi144@gmail.com>
2024-03-27 12:36:10 +0530

    ..

--- LoanApplication/src/main/java/com/loan/app/service/LoanSacService.java
+++ LoanApplication/src/main/java/com/loan/app/service/LoanSacService.java
@@ -1,10 +1,10 @@
 package com.loan.app.service;
 
-import java.util.List;
+
 import java.util.Optional;
 
 import com.loan.app.model.CustomerDetails;
-import com.loan.app.model.Sanction;
+
 
 public interface LoanSacService {
 
